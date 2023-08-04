a98ec7da413222e007a49d236da63728a4037c00
Alexandra Stepanova <ale.stepanova@icloud.com>
2023-08-04 16:38:22 +0300

    refactor: removed creevey

--- .creevey/config.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  screenDir: path.join(__dirname, '../tests/referenceImages'),
-  reportDir: path.join(__dirname, '../tests/report'),
-  diffOptions: { threshold: 0.2 },
-}
